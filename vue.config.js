a4aba9764b93e01db9a88c9e04e4bccc13166db0
wnsh3131@naver.com <wnsh3131@naver.com>
2021-02-19 18:17:32 +0900

    vue Config Update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/ronaldoro/ProjectT-psychological/'
+    ? 'https://github.com/ronaldoro/ProjectT-psychological/tree/master/docs/'
     : '/',
     outputDir: 'docs'
 }
