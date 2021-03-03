f8f2407be70f8c111b137811bf10f9a60263d604
wnsh3131@naver.com <wnsh3131@naver.com>
2021-03-03 22:55:54 +0900

    Home Layout Edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="HomebackGround">
         <div>
-            <h1 class="qustionText">1번 질문입니다.</h1>
+            <h1 class="qustionText">{{questionString}}</h1>
         </div>
         <div>
             <button class="answerNonvisual answerPen answerBrush" type="button" @click="answer1">{{answerString1}}</button>  
         </div>
         <div>
-            <button class="answerNonvisual answerPen" type="button" @click="answer2">{{answerString2}}</button>
+            <button class="answer2Nonvisual answerPen" type="button" @click="answer2" @mouseover="over2">{{answerString2}}</button>
         </div>
     </div>
 </template>
@@ -17,8 +17,10 @@ export default {
     data() {
         return {
             answerIndex: "1",
+            questionString: "1번 질문입니다.",
             answerString1: "1-1번 답변을 선택합니다.",
             answerString2: "1-2번 답변을 선택합니다.",
+            answerClass: "answerNonvisual answerPen"
         }
     },
     methods: {
@@ -30,7 +32,7 @@ export default {
             }
 
             this.answerIndex = integerAnswer;
-            
+            this.questionString = this.answerIndex + "번 질문입니다."
             this.answerString1 = this.answerIndex + "-1번 답변을 선택합니다."
             this.answerString2 = this.answerIndex + "-2번 답변을 선택합니다."
         },
@@ -42,9 +44,13 @@ export default {
             }
 
             this.answerIndex = integerAnswer;
-
+            this.questionString = this.answerIndex + "번 질문입니다."
             this.answerString1 = this.answerIndex + "-1번 답변을 선택합니다."
             this.answerString2 = this.answerIndex + "-2번 답변을 선택합니다."
+        },
+
+        over2() {
+            
         },
     }
     
@@ -57,8 +63,16 @@ export default {
     background-color: #FFAFE6;
 }
 
+.qustionVacancy{
+    position: relative;
+    padding: 10%;
+}
+
 .qustionText{
     text-align: center;
+    padding-top: 10%;
+    padding-bottom: 3rem;
+    font-family:cursive
 }
 
 .answerNonvisual {
@@ -66,13 +80,43 @@ export default {
     position:relative;
     width:40%;
     height:100%;
+    padding:1rem;
+
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 16pt;
+}
+
+.answer2Nonvisual {
+    left: 30%;
+    position:relative;
+    width:40%;
+    height:100%;
+    padding:1rem;
+
+    margin-top:2rem;
+    margin-bottom: 20rem;
+
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 16pt;
 }
 
 .answerPen {
-  border: none;
-  border-radius: 2px;
+  border:thistle;
+  outline-style: dotted;
+  outline-width: 2px;
+  border-radius: 5px;
+}
+
+.answerOverPen {
+  border:black;
+  outline-style: dotted;
+  outline-width: 200px;
+  border-radius: 5px;
 }
 .answerBrush {
   background-color: #FFEFD5;
+}
+.answer2Brush {
+  background-color: #FFD1B7;
 }
 </style>
